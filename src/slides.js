e84b0f5043f16da5a759b42ee887b951f26db640
Drakon <alexxr654@gmail.com>
2021-03-21 23:33:34 +0200

    small change

--- src/slides.js
+++ src/slides.js
@@ -1,42 +1,32 @@
 export const slides = [
   {
-    title: 'Slide 1',
-    image: 'image1.jpg'
+    title: 'Slide 1'
   },
   {
-    title: 'Slide 2',
-    image: 'image2.jpg'
+    title: 'Slide 2'
   },
   {
-    title: 'Slide 3',
-    image: 'image2.jpg'
+    title: 'Slide 3'
   },
   {
-    title: 'Slide 4',
-    image: 'image2.jpg'
+    title: 'Slide 4'
   },
   {
-    title: 'Slide 5',
-    image: 'image2.jpg'
+    title: 'Slide 5'
   },
   {
-    title: 'Slide 6',
-    image: 'image2.jpg'
+    title: 'Slide 6'
   },
   {
-    title: 'Slide 7',
-    image: 'image1.jpg'
+    title: 'Slide 7'
   },
   {
-    title: 'Slide 8',
-    image: 'image1.jpg'
+    title: 'Slide 8'
   },
   {
-    title: 'Slide 9',
-    image: 'image1.jpg'
+    title: 'Slide 9'
   },
   {
-    title: 'Slide 10',
-    image: 'image1.jpg'
+    title: 'Slide 10'
   }
 ]
